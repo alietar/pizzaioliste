62eb57bdf6981470b422dc2736615cf71c9a04dc
alietar <ajm.lietar@gmail.com>
2024-02-18 16:49:30 +0100

    Added filter to admin page

--- website/js/admin.js
+++ website/js/admin.js
@@ -1,6 +1,7 @@
 tableHeaders = document.querySelector('thead tr');
 tableBody = document.querySelector('tbody');
 
+let SOS = {}
 
 async function gatherSOS() {
   const response = await fetch("/api/admin", {
@@ -15,23 +16,62 @@ async function gatherSOS() {
     return
   }
 
-  const SOS = await response.json();
+  SOS = await response.json();
       
-  displayTable(SOS);
+  displayTable(SOS, ["0", "0", "0", "0"]);
 }
 
 
-function displayTable(content) {
+function displayTable(content, filter) {
   format = content['format']
   sos = content['sos']
+
+  tableHeaders.innerHTML = "";
+  tableBody.innerHTML = "";
 
   format.forEach((title) => {
     columnHeader = document.createElement("th");
     columnHeader.innerHTML = title;
     tableHeaders.appendChild(columnHeader);
   });
+    
+  filterButton = document.createElement("th");
+  filterButton.innerHTML = `<a id="filter-btn"><span class="material-symbols-outlined">filter_alt</span></a>`;
+  tableHeaders.appendChild(filterButton);
+
+  document.getElementById('filter-btn').addEventListener('click', function() {
+    document.getElementById('filter-container').classList.toggle('_hidden');
+  });
   
   sos.forEach((row) => {
+    // day
+    if (filter[0] !== "0") {
+      if (row[7].charAt(0) !== filter[0]) {
+        return
+      }
+    }
+    
+    // timeslot
+    if (filter[1] !== "0") {
+      if (row[7].charAt(2) !== filter[1]) {
+        return
+      }
+    }
+    
+    // bat
+    if (filter[2] !== "0") {
+      if (row[8] !== filter[2]) {
+        return
+      }
+    }
+    
+    // floor
+    if (filter[3] !== "0") {
+      if (row[9].toString().charAt(0) !== filter[3]) {
+        return
+      }
+    }
+
     rowElement = document.createElement('tr');
     
     row.forEach((text, i) => {
@@ -60,6 +100,8 @@ function displayTable(content) {
 
       rowElement.appendChild(cell);
     });
+      
+    rowElement.appendChild(document.createElement("td"));
 
     tableBody.appendChild(rowElement);
   });
@@ -67,3 +109,15 @@ function displayTable(content) {
 
 
 gatherSOS();
+
+
+document.querySelector('form').addEventListener('submit', async function(event){
+  event.preventDefault()
+
+  form = document.querySelector('form')
+  let formData = new FormData(form);
+  
+  displayTable(SOS, [formData.get('day'), formData.get('hour'), formData.get('bat'), formData.get('floor')]);
+
+  document.querySelector('form').classList.add('_hidden');
+});
